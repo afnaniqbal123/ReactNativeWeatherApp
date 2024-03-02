1c672a4609ae4585496eb54e0943c8c4ca7ee9ed
Afnan Iqbal <afnaniqbal123.ai@gmail.com>
2024-03-02 18:50:13 +0500

    afnan

---
+++ src/services/weatherApi.js
@@ -0,0 +1,15 @@
+// src/services/weatherApi.js
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const apiKey = '462691efff054d478a7140508242302';
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.weatherapi.com/v1/' }),
+  endpoints: (builder) => ({
+    getWeather: builder.query({
+      query: (city) => `current.json?key=${apiKey}&q=${city}`,
+    }),
+  }),
+});
+
+export const { useGetWeatherQuery } = api;
