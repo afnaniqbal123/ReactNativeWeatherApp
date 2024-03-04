a99dae43ae353b4e13d84ac6c7cd54b59d0e3e18
Afnan Iqbal <afnaniqbal123.ai@gmail.com>
2024-03-04 20:41:16 +0500

    afnan

--- src/component/Weather.js
+++ src/component/Weather.js
@@ -1,109 +1,119 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, Image, ActivityIndicator, Switch, FlatList, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Image, ActivityIndicator, ScrollView, ImageBackground, TouchableOpacity } from 'react-native';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { styles } from '../styles/styles';
 import { StatusBar } from 'expo-status-bar';
-import { CalendarDaysIcon, MagnifyingGlassIcon, XCircleIcon } from "react-native-heroicons/outline"; // Import XIcon
-
+import { CalendarDaysIcon, MagnifyingGlassIcon, XCircleIcon } from "react-native-heroicons/outline";
 import * as Location from 'expo-location';
 
 const apiKey = '462691efff054d478a7140508242302';
 
 const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://api.weatherapi.com/v1/' }),
-    endpoints: (builder) => ({
-        getWeather: builder.query({
-            query: (city) => `current.json?key=${apiKey}&q=${city}`,
-        }),
-        getForecast: builder.query({
-            query: (city) => `forecast.json?key=${apiKey}&q=${city}&days=7`,
-        }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://api.weatherapi.com/v1/' }),
+  endpoints: (builder) => ({
+    getWeather: builder.query({
+      query: (city) => `current.json?key=${apiKey}&q=${city}`,
     }),
+    getForecast: builder.query({
+      query: (city) => `forecast.json?key=${apiKey}&q=${city}&days=7`,
+    }),
+  }),
 });
 
 const { useGetWeatherQuery, useGetForecastQuery } = api;
 
 const Weather = () => {
-    const [city, setCity] = useState('');
-    const [isCelsius, setIsCelsius] = useState(true);
-    const { data: weatherData, error: weatherError, isLoading: weatherLoading, refetch: refetchWeather } = useGetWeatherQuery(city);
-    const { data: forecastData, error: forecastError, isLoading: forecastLoading, refetch: refetchForecast } = useGetForecastQuery(city);
+  const [city, setCity] = useState('');
+  const [isCelsius, setIsCelsius] = useState(true);
+  const { data: weatherData, error: weatherError, isLoading: weatherLoading, refetch: refetchWeather } = useGetWeatherQuery(city);
+  const { data: forecastData, error: forecastError, isLoading: forecastLoading, refetch: refetchForecast } = useGetForecastQuery(city);
 
-    const getDefaultCity = async () => {
-        try {
-            const { status } = await Location.requestForegroundPermissionsAsync();
-            if (status === 'granted') {
-                const location = await Location.getCurrentPositionAsync({});
-                const { latitude, longitude } = location.coords;
-                const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`);
-                const data = await response.json();
-                setCity(data.location.name);
-            }
-        } catch (error) {
-            console.error('Error fetching default city:', error);
-            throw error; // Throw the error to be caught in handleClear
-        }
-    };
+  const getDefaultCity = async () => {
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status === 'granted') {
+        const location = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = location.coords;
+        const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${latitude},${longitude}`);
+        const data = await response.json();
+        setCity(data.location.name);
+      }
+    } catch (error) {
+      console.error('Error fetching default city:', error);
+      throw error; // Throw the error to be caught in handleClear
+    }
+  };
 
-    useEffect(() => {
-        getDefaultCity();
-    }, []);
+  useEffect(() => {
+    getDefaultCity();
+  }, []);
 
-    const handleSearch = async () => {
-        if (city.trim() !== '') {
-            try {
-                await refetchWeather();
-                await refetchForecast();
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-            }
-        }
-    };
+  const handleSearch = async () => {
+    if (city.trim() !== '') {
+      try {
+        await refetchWeather();
+        await refetchForecast();
+      } catch (error) {
+        console.error('Error fetching weather data:', error);
+      }
+    }
+  };
 
-    const handleClear = async () => {
-        try {
-            await getDefaultCity();
-            await refetchWeather();
-            await refetchForecast();
-        } catch (error) {
-            console.error('Error clearing and fetching default city:', error);
-        }
-    };
-    return (
-<ScrollView>
-        <View style={styles.container}>
-            <StatusBar style='light' />
-            <ImageBackground blurRadius={40} style={styles.backgroundImage} source={require("../../assets/10.png")} className='Absolute w-full h-full'>
-                <View style={styles.card}>
-                    <View style={styles.serachbar}>
+  const handleClear = async () => {
+    try {
+      await getDefaultCity();
+      await refetchWeather();
+      await refetchForecast();
+    } catch (error) {
+      console.error('Error clearing and fetching default city:', error);
+    }
+  };
+  
+  const getFormattedTime = (timestamp) => {
+    const date = new Date(timestamp * 1000); // Convert timestamp to milliseconds
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  };
 
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Enter city"
-                            value={city}
-                            onChangeText={(text) => setCity(text)}
-                        />
-                        <TouchableOpacity style={styles.iconsearch1} onPress={handleClear}>
-                            <XCircleIcon size="30" color="white" />
-                        </TouchableOpacity>
-                        <TouchableOpacity style={styles.iconsearch} onPress={handleSearch}>
-                            <MagnifyingGlassIcon size="25" color="white" />
-                        </TouchableOpacity>
-                    </View>
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style='light' />
+        <ImageBackground blurRadius={40} style={styles.backgroundImage} source={require("../../assets/10.png")} className='Absolute w-full h-full'>
+          <View style={styles.card}>
+            {/* Search Bar */}
+            <View style={styles.searchBar}>
+              <TextInput
+                style={styles.input}
+                placeholder="Enter city"
+                value={city}
+                onChangeText={(text) => setCity(text)}
+              />
+              <TouchableOpacity style={styles.iconButton} onPress={handleClear}>
+                <XCircleIcon size={30} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.iconButton} onPress={handleSearch}>
+                <MagnifyingGlassIcon size={25} color="white" />
+              </TouchableOpacity>
+            </View>
 
-                    {(weatherLoading || forecastLoading) && (
-                        <ActivityIndicator size="large" color="blue" />
-                    )}
+            {/* Loading Indicator */}
+            {weatherLoading || forecastLoading ? (
+              <ActivityIndicator size="large" color="blue" />
+            ) : (
+              <>
+                {/* Weather and Forecast Content */}
+                {weatherError && <Text style={styles.errorText}>Weather Error: {weatherError.message}</Text>}
 
-                    {weatherError && <Text style={styles.errorText}>Error: {weatherError.message}</Text>}
-
-                    {weatherData && forecastData && (
-                        <View style={styles.weatherContainer}>
-                            <Text style={styles.citytext}> {weatherData.location.name}<Text style={{ fontSize: 20 }}> ,{weatherData.location.country}</Text></Text>
-                            <View style={styles.circularBackground}>
+                {weatherData && forecastData && (
+                  <View style={styles.weatherContainer}>
+                    {/* Current Weather */}
+                    <Text style={styles.citytext}> {weatherData.location.name}<Text style={{ fontSize: 20 }}>, {weatherData.location.country}</Text></Text>
+                    <View style={styles.circularBackground}>
                                 <Image source={{ uri: 'https:' + weatherData.current.condition.icon }} style={styles.weatherImage} />
                                 <TouchableOpacity onPress={() => setIsCelsius(!isCelsius)} >
-                                    <Text style={styles.Temperature}>
+                                    <Text style={styles.temperature}>
                                         {isCelsius ? weatherData.current.temp_c : weatherData.current.temp_f}°
                                         {isCelsius ? 'C' : 'F'}
                                     </Text>
@@ -130,43 +140,43 @@ const Weather = () => {
                             </View>
                             <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 10 }}>
                             </View>
+                    
+                    {/* Daily Forecast */}
+                    <View style={{ marginTop: 40 }}>
+                      <View style={{ flexDirection: 'row' }}>
+                        <CalendarDaysIcon size="25" color="white" style={{ alignSelf: "center" }} />
+                        <Text style={{ textAlign: 'center', fontSize: 20, marginLeft: 10, color: 'white' }}>Daily Forecast:</Text>
+                      </View>
 
-                          
-                                <View style={{ marginTop: 40 }}>
-                                    <View style={{ flexDirection: 'row' }}>
-                                        <CalendarDaysIcon size="25" color="white" style={{ alignSelf: "center" }} />
-                                        <Text style={{ textAlign: 'center', fontSize: 20, marginLeft: 10, color: 'white' }}>Daily Forecast:</Text>
-                                    </View>
-
-                                    <ScrollView horizontal contentContainerStyle={{ paddingHorizontal: 15 }} showsHorizontalScrollIndicator={false}>
-                                        {
-                                            forecastData?.forecast?.forecastday?.map((item , index) => {
-                                                let date = new Date(item.date);
-                                                let option = {weekday :'long'}
-                                                let dayname = date.toLocaleDateString('en-us',option)
-                                                dayname = dayname.split(',')[0]
-                                            return (
-                                            <View style={styles.forecastbox} key={index}>
-                                                <Image source={{ uri: 'https:' + item?.day?.condition?.icon }} style={styles.forecasticon} />
+                      <ScrollView horizontal contentContainerStyle={{ paddingHorizontal: 15 }} showsHorizontalScrollIndicator={false}>
+                        {forecastData?.forecast?.forecastday?.map((item, index) => {
+                          let date = new Date(item.date);
+                          let option = { weekday: 'long' }
+                          let dayname = date.toLocaleDateString('en-us', option)
+                          dayname = dayname.split(',')[0]
+                          return (
+                            <View style={styles.forecastbox} key={index}>
+                            <Image source={{ uri: 'https:' + item?.day?.condition?.icon }} style={styles.forecasticon} />
                                                 <Text style={{color:'white'}}>{dayname}</Text>
                                                 <Text style={{color:'white'}}>
                                                  {isCelsius ? item?.day?.avgtemp_c : item?.day?.avgtemp_f}°
                                                     {isCelsius ? 'C' : 'F'}
                                                 </Text>
                                                 <Text style={{color:'white'}}>{item?.day?.condition?.text}</Text>
-                                            </View>
-                                            )
-                                        })}
-                                    </ScrollView>
-                                </View>
-                        </View>
-                    )}
-                </View>
-            </ImageBackground>
-        </View>
-        </ScrollView>
-
-    );
+                            </View>
+                          )
+                        })}
+                      </ScrollView>
+                    </View>
+                  </View>
+                )}
+              </>
+            )}
+          </View>
+        </ImageBackground>
+      </View>
+    </ScrollView>
+  );
 };
 
 export default Weather;
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -2,43 +2,34 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    card: {
+  card: {
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop:50,
-    
+    marginTop: 50,
   },
   container: {
-  height:900,
+    height: 900,
   },
   input: {
-    textAlign:'center',
+    textAlign: 'center',
+    flex: 1,
+    paddingHorizontal: 8,
   },
-  serachbar: {
+  searchBar: {
     height: 40,
     borderColor: 'white',
     borderWidth: 1,
     borderRadius: 20,
     backgroundColor: "rgba(255, 255, 255, 0.3)",
-    flexDirection:'row',
+    flexDirection: 'row',
     marginBottom: 10,
     width: '80%',
-    justifyContent:'space-between',
-    paddingHorizontal:10,
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
   },
-  iconsearch: {
-    alignSelf:'center',
-  },
-  iconsearch1: {
-    alignSelf:'center',
-    marginLeft:150,
-  },
-  searchbutton: {
-    backgroundColor: 'black',
-    color: 'white',
-    padding: 10,
-    borderRadius: 5,
-    width: '100%',
+  iconButton: {
+    alignSelf: 'center',
   },
   weatherContainer: {
     marginTop: 20,
@@ -50,43 +41,42 @@ export const styles = StyleSheet.create({
     fontSize: 30,
     marginBottom: 10,
   },
-  Temperature: {
+  temperature: {
     fontSize: 30,
     marginBottom: 10,
-    textAlign:'center',
-    color:'white',
-    fontWeight:'bold',
+    textAlign: 'center',
+    color: 'white',
+    fontWeight: 'bold',
   },
   condition: {
     fontSize: 20,
     marginBottom: 10,
-    textAlign:'center',
-    color:'white',
+    textAlign: 'center',
+    color: 'white',
   },
   circularBackground: {
-    backgroundColor: 'rgba(255, 255, 255, 0.3)', // Opacity white color
-    borderRadius: 150, // Adjust the value as needed
-    padding: 20, // Adjust the value as needed
+    backgroundColor: 'rgba(255, 255, 255, 0.3)',
+    borderRadius: 150,
+    padding: 20,
     alignItems: 'center',
     justifyContent: 'center',
-    margin: 20, // Adjust the value as needed to separate it from other elements
+    margin: 20,
   },
   citytext: {
     fontSize: 30,
-    textAlign:'center',
-    color:'white',
+    textAlign: 'center',
+    color: 'white',
   },
-  box:{
-    flexDirection:'row',
-    height:50,
-    width:70,
-    alignItems:'center',
-    justifyContent:'space-between',
-
+  weatherDetailBox: {
+    flexDirection: 'row',
+    height: 50,
+    width: 70,
+    alignItems: 'center',
+    justifyContent: 'space-between',
   },
-  forecastbox:{
+  forecastbox: {
     height:130,
-    width:90,
+    width:100,
     alignItems:'center',
     justifyContent:'space-between',
     backgroundColor: 'rgba(255, 255, 255, 0.3)',
@@ -96,31 +86,29 @@ export const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 20,
   },
-
-  forecasticon:{
+  forecasticon: {
     width: 70,
     height: 70,
     resizeMode: 'cover',
-    alignSelf:'center',
+    alignSelf: 'center',
   },
   weatherImage: {
     width: 150,
     height: 150,
     resizeMode: 'cover',
-    alignSelf:'center',
+    alignSelf: 'center',
   },
   icon: {
     width: 40,
     height: 40,
     resizeMode: 'center',
-    alignSelf:'center',
+    alignSelf: 'center',
   },
   backgroundImage: {
     flex: 1,
     width: '100%',
     resizeMode: 'cover',
   },
-
   errorText: {
     color: 'red',
     marginBottom: 10,
